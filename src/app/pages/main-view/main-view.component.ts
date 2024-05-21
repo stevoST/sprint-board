51050959c720c35d2dc705bca1f396cb1e9f854e
Stefan Starosta <stefan.starosta@gmail.com>
2024-05-21 09:15:53 +0200

    added reading column names and content from application

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -42,9 +42,6 @@ export class MainViewComponent {
       'Walk dog'
     ]),
   ])
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
